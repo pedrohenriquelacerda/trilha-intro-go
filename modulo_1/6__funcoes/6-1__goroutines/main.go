50b288ea296eeeac5331be7818894c1dc812fcf8
Victor Octavio <victoroctavio4@gmail.com>
2025-06-30 18:46:01 -0300

    Add goroutine example

--- modulo_1/6__funcoes/6-1__goroutines/main.go
+++ modulo_1/6__funcoes/6-1__goroutines/main.go
@@ -12,8 +12,14 @@ func sayHello() {
 }
 
 func main() {
-	go sayHello() // inicia uma goroutine
-	fmt.Println("Olá do main!")
-	time.Sleep(2 * time.Second) // espera para a goroutine executar
-	fmt.Println("Encerrando a execução")
+	go countTo10()
+	sayHello()
+	time.Sleep(100 * time.Second)
+}
+
+func countTo10() {
+	for i := 1; i <= 10; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(i)
+	}
 }
