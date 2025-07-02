f6ff95e99b5c89c21a5b78c7594562a5bc0bcbab
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2025-07-01 21:36:24 -0300

    Implementa exemplo de array no main.go

--- modulo_1/2__arrays/main.go
+++ modulo_1/2__arrays/main.go
@@ -1,6 +1,12 @@
 package main
 
-// Crie um array de 3 inteiros com os valores 10, 20, 30 
+// Crie um array de 3 inteiros com os valores 10, 20, 30
 // e imprima todos valores do array.
 
-func main(){}
+import "fmt"
+
+func main() {
+	numeros := [3]int{10, 20, 30}
+
+	fmt.Println(numeros)
+}
