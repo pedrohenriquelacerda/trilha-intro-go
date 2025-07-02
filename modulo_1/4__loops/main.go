661da1c0fc86d31ad95770d7ead7ca99be5a7a0b
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2025-07-01 21:36:44 -0300

    Implementa exemplo de uso de for clássico e for-range com array

--- modulo_1/4__loops/main.go
+++ modulo_1/4__loops/main.go
@@ -3,6 +3,18 @@ package main
 // Inicialize um array com 3 elementos, utilize o for() clássico para
 // imprimir todos os elementos na tela e após isso faça a mesma coisa com for-range()
 
+import "fmt"
 
+func main() {
+	arr := [3]int{1, 2, 3}
 
-func main(){}
+	// Usando for clássico
+	for i := 0; i < len(arr); i++ {
+		println(arr[i])
+	}
+
+	// Usando for-range
+	for index, value := range arr {
+		fmt.Printf("O indíce é %d e o valor é %d\n", index, value)
+	}
+}
