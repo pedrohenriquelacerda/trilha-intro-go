67806b41d48e6404f413a0943915e28ad7ef6f0e
Pedro Henrique Rosa Lacerda da Silva <108750566+pedrohenriquelacerda@users.noreply.github.com>
2025-07-01 21:36:56 -0300

    Implementa lógica de aprovação de alunos com condicionais

--- modulo_1/5__condicoes/main.go
+++ modulo_1/5__condicoes/main.go
@@ -3,4 +3,23 @@ package main
 // Crie uma variável 'nota', logo após crie uma condição para aprovar alunos
 // 'nota' maior ou igual a 7 imprime "Aprovado", caso contrário imprime "Reprovado"
 
-func main(){}
+func main() {
+	var nota float64 = 10
+
+	if nota >= 7 {
+		println("Aprovado")
+	} else {
+		println("Reprovado")
+	}
+
+	switch nota {
+	case 10:
+		println("Aprovadasso")
+	case 9, 8:
+		println("Aprovado com nota alta")
+	case 7:
+		println("Aprovado")
+	default:
+		println("Reprovado")
+	}
+}
